aad726948ef3c16e5b41ca7397d95b75e881e98d
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-11-23 18:02:29 +0000

    Update google_map_url to google_maps_url

--- conferences/migrations/2_add_venue_location.up.sql
+++ conferences/migrations/2_add_venue_location.up.sql
@@ -6,7 +6,7 @@ CREATE TABLE venue(
   description TEXT NOT NULL,
   address TEXT NOT NULL,
   directions TEXT NOT NULL, 
-  google_map_url TEXT NOT NULL,
+  google_maps_url TEXT NOT NULL,
   capacity int
 );
 
@@ -15,14 +15,15 @@ INSERT INTO venue (
   description,
   address,
   directions,
-  google_map_url,
-  capacity) VALUES (
-    'GopherTown', 
-    'The virtual GopherCon meeting space!', 
-    'GopherTown, GopherVille, Gophera, 12345', 
-    'Do not follow the beavers', 
-    'https://gophercon.com', 
-    2500
+  google_maps_url,
+  capacity
+) VALUES (
+  'GopherTown', 
+  'The virtual GopherCon meeting space!', 
+  'GopherTown, GopherVille, Gophera, 12345', 
+  'Do not follow the beavers', 
+  'https://gophercon.com', 
+  2500
 );
 
 INSERT INTO venue (
@@ -30,14 +31,15 @@ INSERT INTO venue (
   description, 
   address, 
   directions, 
-  google_map_url, 
-  capacity) VALUES (
-    'Disneyworld', 
-    'The Walt Disney World Dolphin Resort', 
-    '1500 Epcot Resorts Boulevard, Lake Buena Vista, Florida 32830',
-    'Follow the yellow brick road', 
-    'https://goo.gl/maps/AwZVpoSXzbzgJkoL8', 
-    3000
+  google_maps_url, 
+  capacity
+) VALUES (
+  'Disneyworld', 
+  'The Walt Disney World Dolphin Resort', 
+  '1500 Epcot Resorts Boulevard, Lake Buena Vista, Florida 32830',
+  'Follow the yellow brick road', 
+  'https://goo.gl/maps/AwZVpoSXzbzgJkoL8', 
+  3000
 );
 
 CREATE TABLE location(
@@ -46,7 +48,7 @@ CREATE TABLE location(
   description TEXT NOT NULL, 
   address TEXT NOT NULL,
   directions TEXT NOT NULL,
-  google_map_url TEXT NULL,
+  google_maps_url TEXT NULL,
   capacity INT NOT NULL,
   venue_id SERIAL NOT NULL REFERENCES venue(id)
 );
@@ -57,17 +59,17 @@ INSERT INTO location (
   description, 
   address, 
   directions, 
-  google_map_url, 
+  google_maps_url, 
   capacity, 
   venue_id
-  ) VALUES (
-    'The Den', 
-    'Suitable for napping', 
-    'GopherTown, The Internet, 418', 
-    'Left at the crab, right at the penguin, straight down the hole', 
-    'https://gophercon.com/gopher-den', 
-    450, 
-    1
+) VALUES (
+  'The Den', 
+  'Suitable for napping', 
+  'GopherTown, The Internet, 418', 
+  'Left at the crab, right at the penguin, straight down the hole', 
+  'https://gophercon.com/gopher-den', 
+  450, 
+  1
 );
 
 INSERT INTO location (
@@ -75,17 +77,17 @@ INSERT INTO location (
   description, 
   address, 
   directions, 
-  google_map_url, 
+  google_maps_url, 
   capacity, 
   venue_id
-  ) VALUES (
-    'Cinderella Castle', 
-    'Where princesses give speeches', 
-    'Cinderella Castle, Orlando, Florida 32836, USA', 
-    'Down the rabbit hole', 
-    'https://goo.gl/maps/G668N9bK14BbryK36', 
-    300, 
-    2
+) VALUES (
+  'Cinderella Castle', 
+  'Where princesses give speeches', 
+  'Cinderella Castle, Orlando, Florida 32836, USA', 
+  'Down the rabbit hole', 
+  'https://goo.gl/maps/G668N9bK14BbryK36', 
+  300, 
+  2
 );
 
 
@@ -113,18 +115,18 @@ INSERT INTO conference_slot(
   available_to_public, 
   conference_id, 
   location_id
-  ) VALUES (
-    'Flappy Gopher Round 2', 
-    'Will they beat it this time?', 
-    50, 
-    150, 
-    '2021-11-09 17:00:00+00', 
-    '2021-11-09 21:00:00+00', 
-    '2021-05-09 17:00:00+00', 
-    '2021-11-04 17:00:00+00', 
-    true, 
-    2, 
-    2
+) VALUES (
+  'Flappy Gopher Round 2', 
+  'Will they beat it this time?', 
+  50, 
+  150, 
+  '2021-11-09 17:00:00+00', 
+  '2021-11-09 21:00:00+00', 
+  '2021-05-09 17:00:00+00', 
+  '2021-11-04 17:00:00+00', 
+  true, 
+  2, 
+  2
 );
 
 UPDATE conference_slot 
