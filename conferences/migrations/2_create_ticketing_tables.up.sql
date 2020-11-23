0d7fc6d7f77c85434c4219f731b5f9ac68782dc0
Horacio Duran <horacio.duran@gmail.com>
2020-11-23 13:33:10 -0300

    Add nulity constraints to various columns

--- conferences/migrations/2_create_ticketing_tables.up.sql
+++ conferences/migrations/2_create_ticketing_tables.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE slot_claim (
     id SERIAL PRIMARY KEY,
-    conference_slot_id INT REFERENCES conference_slot(id), 
+    conference_slot_id INT NOT NULL REFERENCES conference_slot(id) , 
     ticket_id UUID CONSTRAINT ticket_id_is_unique UNIQUE,
     redeemed BOOLEAN
 );
@@ -12,8 +12,8 @@ CREATE TABLE attendee (
 );
 
 CREATE TABLE attendee_to_slot_claims (
-    attendee_id INT REFERENCES attendee(id),
-    slot_claim_id INT CONSTRAINT slot_claim_id_is_unique UNIQUE,
+    attendee_id INT NOT NULL REFERENCES attendee(id),
+    slot_claim_id INT NOT NULL CONSTRAINT slot_claim_id_is_unique UNIQUE,
     FOREIGN KEY (slot_claim_id) REFERENCES slot_claim(id)
 );
 
@@ -24,34 +24,34 @@ CREATE TABLE claim_payment (
 
 CREATE TABLE payment_method_money (
     id SERIAL PRIMARY KEY,
-    amount INTEGER,
-    ref TEXT
+    amount INTEGER NOT NULL,
+    ref TEXT NOT NULL
 );
 
 CREATE TABLE payment_method_money_to_claim_payment (
-    payment_method_money_id INT UNIQUE REFERENCES payment_method_money(id),
-    claim_payment_id INT REFERENCES claim_payment(id)
+    payment_method_money_id INT NOT NULL UNIQUE REFERENCES payment_method_money(id),
+    claim_payment_id INT NOT NULL REFERENCES claim_payment(id)
 );
 
 CREATE TABLE payment_method_credit_note (
     id SERIAL PRIMARY KEY,
-    amount INTEGER,
+    amount INTEGER NOT NULL,
     detail TEXT
 );
 
 CREATE TABLE payment_method_credit_note_to_claim_payment (
-    payment_method_credit_note_id INT UNIQUE REFERENCES payment_method_credit_note(id),
-    claim_payment_id INTREFERENCES claim_payment(id) 
+    payment_method_credit_note_id INT NOT NULL UNIQUE REFERENCES payment_method_credit_note(id),
+    claim_payment_id INT NOT NULL REFERENCES claim_payment(id) 
 );
 
 CREATE TABLE payment_method_conference_discount (
     id SERIAL PRIMARY KEY,
-    amount INTEGER,
+    amount INTEGER  NOT NULL,
     detail TEXT
 );
 
 CREATE TABLE payment_method_conference_discount_to_claim_payment (
-    payment_method_conference_discount_id INT UNIQUE REFERENCES payment_method_conference_discount(id),
-    claim_payment_id INT REFERENCES claim_payment(id)
+    payment_method_conference_discount_id INT  NOT NULL UNIQUE REFERENCES payment_method_conference_discount(id),
+    claim_payment_id INT  NOT NULL REFERENCES claim_payment(id)
 );
 
