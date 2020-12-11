5c145073684c7fe0dbd63ac8a1d3b743de3bdcc1
Brian Ketelsen <mail@bjk.fyi>
2020-12-11 10:54:12 -0500

    validate keys

--- conferences/auth.go
+++ conferences/auth.go
@@ -2,14 +2,18 @@ package conferences
 
 import (
 	"context"
+	"encoding/json"
+	"log"
 
 	"encore.dev/beta/auth"
-	"encore.dev/rlog"
+	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/oauth2"
 )
 
 var secrets struct {
-	JWTKey string
+	JWTKey     string
+	OKTAPUBKEY string
 }
 
 // Data is the structure that Encore returns with information about the authenticated user
@@ -21,20 +25,52 @@ type Data struct {
 	UserID           string   `json:"userId"`
 	UserRoles        []string `json:"userRoles"`
 }
+type info struct {
+	IDToken   string          `json:"id"`
+	AuthToken string          `json:"auth"`
+	UserInfo  json.RawMessage `json:"ui"`
+}
 
 // VerifyToken accepts a JWT token and returns a UserID and User Data, or an error.
 // Return a zero-value UID for Unauthorized, return a non-nil error for a 500 error
 // encore:authhandler
 func VerifyToken(ctx context.Context, token string) (auth.UID, *Data, error) {
-	rlog.Info("decrypting token", "token", token)
-	tok, err := jwt.Parse(token, nil)
-	if tok == nil {
-		return auth.UID(""), nil, err
+	provider, err := oidc.NewProvider(ctx, "https://dev-7217861.okta.com")
+	if err != nil {
+		log.Println("provider create error", err)
+		return "", nil, err
 	}
-	claims, _ := tok.Claims.(jwt.MapClaims)
-	d := mapClaims(claims)
+	log.Println(token)
+	var i info
+	err = json.Unmarshal([]byte(token), &i)
+
+	if err != nil {
+		log.Println("unmarshal token data struct", err)
+		return "", nil, err
+	}
+	var verifier = provider.Verifier(&oidc.Config{ClientID: "0oa26dc0cgcjzHwsJ5d6"})
+	idtok, err := verifier.Verify(ctx, i.IDToken)
+	if err != nil {
+		log.Println("verify token error: ", err)
+		return "", nil, err
+	}
+	log.Println("idtok", idtok)
+	var oidcToken oauth2.Token
+	oidcToken.AccessToken = string(i.AuthToken)
+	oidcToken.TokenType = "Bearer"
+
+	ui, err := provider.UserInfo(ctx, oauth2.StaticTokenSource(&oidcToken))
+	if err != nil {
+		log.Println("user info error:", err)
+		return "", nil, err
+	}
+
+	log.Println("ui", ui)
+	var d Data
+	ui.Claims(&d)
 	//TODO: actually validate things from the token/claims
-	return auth.UID(d.UserID), d, nil
+	//return auth.UID(d.UserID), d, nil
+	return "", &d, nil
 }
 
 func mapClaims(values jwt.MapClaims) *Data {
--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.15
 
 require (
 	encore.dev v0.7.0
+	github.com/coreos/go-oidc/v3 v3.0.0-alpha.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gofrs/uuid v3.3.0+incompatible
 	github.com/lib/pq v1.8.0
+	github.com/okta/okta-jwt-verifier-golang v1.0.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20201208152858-08078c50e5b5 // indirect
 )
